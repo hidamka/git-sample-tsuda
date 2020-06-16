7cc4107e77b1266cbe56b83cbd78eeeeeaf7f1f5
tsudamon0421 <hikaru.tsuda@rakus.internal>
2020-06-16 12:19:26 +0900

    Memberドメイン作成

---
+++ src/main/java/com/example/domain/Member.java
@@ -0,0 +1,42 @@
+package com.example.domain;
+
+public class Member {
+	
+	private Integer id;
+	private String name;
+	private Integer age;
+	private String email;
+	
+	
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", name=" + name + ", age=" + age + ", email=" + email + "]";
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
